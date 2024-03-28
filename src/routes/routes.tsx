195fe9c0723a2ac4b74e93123cf0335df19043ed
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-03-28 17:31:30 +0530

    #223 Modified Routes to support relative paths

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,10 @@
 import { lazy } from "react";
 
-const Home = lazy(() => import("../pages/Home"));
-const Order = lazy(() => import("../pages/dashboard/order/Order"));
-const VerifyEmail = lazy(() => import("../pages/VerifyEmail"));
-const PlaceOrder = lazy(() => import("../pages/customer/order/Order"));
-const NotFound = lazy(() => import("../pages/NotFound"));
+const Home = lazy(() => import("@app_pages/Home"));
+const Order = lazy(() => import("@app_pages/dashboard/order/Order"));
+const VerifyEmail = lazy(() => import("@app_pages/VerifyEmail"));
+const PlaceOrder = lazy(() => import("@app_pages/customer/order/Order"));
+const NotFound = lazy(() => import("@app_pages/NotFound"));
 
 export const privateRoutes = [
     {
