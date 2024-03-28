4cbfa5524285f669147c555bf417c09a549eb45c
Teran Sarathchandra <Teran.Sarathchandra@softlogiclife.lk>
2024-03-28 23:56:45 +0530

    withCredentials is set to true on the XHR object

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -8,6 +8,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 // Add a request interceptor to add the bearer token to the headers
