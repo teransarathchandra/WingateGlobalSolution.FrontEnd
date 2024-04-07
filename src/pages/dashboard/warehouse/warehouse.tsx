92fe84524d36cc32d03df0bfc1c7acd47e9e39e4
Thisara Nuwanthi <nuwanthithisara9@gmail.com>
2024-04-07 17:35:45 +0530

    #358 Minor Changes in Warehouse

---
+++ src/pages/dashboard/warehouse/warehouse.tsx
@@ -0,0 +1,12 @@
+import WareHouseInfo from "@app_components/dashboard/warehouse/warehouseInfo/WareHouseInfo"
+
+
+const WareHouse = () => {
+  return (
+    <>
+      <WareHouseInfo />
+    </>
+  )
+}
+
+export default WareHouse
