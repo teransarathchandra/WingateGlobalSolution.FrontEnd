0aa1410ac318991cf6896e97b2161bd14e1d1958
Teran Sarathchandra <teran8777@gmail.com>
2024-04-16 01:16:08 +0530

    #185 Order Placement Form Validations Added

--- src/components/forms/order/SenderForm.tsx
+++ src/components/forms/order/SenderForm.tsx
@@ -1,25 +1,39 @@
 import TextField from "@mui/material/TextField";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import shipmentDetailsSchema from "@app_schemas/shipmentDetailsSchema";
 import { StyledForm } from "@app_styles/shared/form.styles";
 import ChooseDestinationImg from '@app_assets/images/customer/ChooseDestination.png'
 import { HeadingSection } from "@app_styles/shared/heading.styles";
 import { FlexRow, ImageContainer } from "@app_styles/shared/commonStyles.style";
+import senderDetailsSchema from "@app_schemas/senderSchema";
+import { forwardRef, useImperativeHandle } from "react";
 
-const SenderForm = () => {
+interface SenderFormProps {
+    onFormSubmit: (data: any, isValid: boolean) => void;
+}
+
+interface SenderFormMethods {
+    submitForm: () => void;
+}
+
+const SenderForm = forwardRef<SenderFormMethods, SenderFormProps>(({ onFormSubmit }, ref) => {
 
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm({
-        resolver: yupResolver(shipmentDetailsSchema), // Define your validation schema
+        resolver: yupResolver(senderDetailsSchema),
+        mode: 'onTouched',
     });
 
-    const onSubmit = (data) => {
-        console.log("Shipment Data", data);
-        // Handle the submission to your backend or state management
+    useImperativeHandle(ref, () => ({
+        submitForm: () => handleSubmit(onSubmit)()
+    }));
+
+    const onSubmit = async (data) => {
+        const isValid = Object.keys(errors).length === 0;
+        onFormSubmit(data, isValid);
     };
 
     return (
@@ -31,72 +45,73 @@ const SenderForm = () => {
             </HeadingSection>
             <TextField
                 label="Name"
+                type="text"
                 variant="outlined"
                 size="small"
                 fullWidth
-                {...register("itemName")}
-                error={!!errors.itemName}
-                helperText={errors.itemName?.message}
+                {...register("name")}
+                error={!!errors.name}
+                helperText={errors.name?.message as string}
                 margin="dense"
             />
             <TextField
                 label="Contact Number"
+                type="number"
                 variant="outlined"
                 size="small"
                 fullWidth
-                {...register("description")}
-                error={!!errors.description}
-                helperText={errors.description?.message}
+                {...register("contactNumber")}
+                error={!!errors.contactNumber}
+                helperText={errors.contactNumber?.message as string}
                 margin="dense"
             />
             <TextField
-                label="Email (Optional)"
+                label="Email Address"
+                type="email"
                 variant="outlined"
                 size="small"
                 fullWidth
-                {...register("description")}
-                error={!!errors.description}
-                helperText={errors.description?.message}
+                {...register("email")}
+                error={!!errors.email}
+                helperText={errors.email?.message as string}
                 margin="dense"
             />
             <TextField
                 label="State"
-                select
+                type="text"
                 variant="outlined"
                 size="small"
                 fullWidth
-                {...register("category")}
-                error={!!errors.category}
-                helperText={errors.category?.message}
+                {...register("state")}
+                error={!!errors.state}
+                helperText={errors.state?.message as string}
                 margin="dense"
             >
-                <option value="">Select a category</option>
             </TextField>
             <TextField
                 label="City"
-                type="number"
+                type="text"
                 variant="outlined"
                 size="small"
                 fullWidth
-                {...register("weight")}
-                error={!!errors.weight}
-                helperText={errors.weight?.message}
+                {...register("city")}
+                error={!!errors.city}
+                helperText={errors.city?.message as string}
                 margin="dense"
             />
             <TextField
                 label="Street"
-                type="number"
+                type="text"
                 variant="outlined"
                 size="small"
                 fullWidth
-                {...register("value")}
-                error={!!errors.value}
-                helperText={errors.value?.message}
+                {...register("street")}
+                error={!!errors.street}
+                helperText={errors.street?.message as string}
                 margin="dense"
             />
-            {/* <ActionButton type="submit">Submit Details</ActionButton> */}
         </StyledForm>
     )
-}
+});
 
 export default SenderForm
