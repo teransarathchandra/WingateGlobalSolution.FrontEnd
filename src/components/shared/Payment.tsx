46045bd4749bb88e94981ad434b3737961fe06b2
Teran Sarathchandra <teran8777@gmail.com>
2024-04-16 15:35:06 +0530

    #422 Payhere Payment Gateway Integration

---
+++ src/components/shared/Payment.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import api from "@app_utils/apiUtils";
+
+const Payment = ({
+    paymentTitle,
+    amount,
+    order_id,
+    currency,
+    customerDetails
+}) => {
+
+    const [paymentStatus, setPaymentStatus] = useState('');
+
+    const fetchHash = async (paymentData) => {
+        const response = await api.post('/payment/generate_hash', paymentData);
+        if (response) {
+            const { data } = await response;
+            return data.hash;
+        } else {
+            throw new Error('Failed to fetch hash from the backend.');
+        }
+    };
+
+    // Function to initiate PayHere payment
+    const initiatePayment = async () => {
+
+        try {
+            const hash = await fetchHash({
+                order_id,
+                amount,
+                currency
+            });
+
+            const payment = {
+                sandbox: true,
+                merchant_id: "1225830",   //change your merchant_id
+                return_url: "http://localhost:5173/order",
+                cancel_url: "http://localhost:5173/order",
+                notify_url: "http://localhost:5173/order", // important but need public url not local host to recive data
+                order_id,
+                items: paymentTitle,
+                amount: amount,
+                currency: "LKR",
+                hash,
+                first_name: customerDetails.firstName,
+                last_name: customerDetails.lastName,
+                email: customerDetails.email,
+                phone: customerDetails.phone,
+                address: customerDetails.address,
+                city: customerDetails.city,
+                country: "Sri Lanka",
+            };
+            console.log(payment);
+            // Start PayHere Payment
+            window.payhere.startPayment(payment);
+
+        } catch (error) {
+            console.error('Error initiating payment:', error);
+            setPaymentStatus('Failed to initiate payment');
+        }
+    };
+
+    window.payhere.onCompleted = function onCompleted(order_id) {
+        console.log("Payment completed. OrderID:" + order_id);
+        // setPaymentSuccess(true);
+        // setSuccess(true); // to do changes in implement page
+        // setOrderID(order_id); // to save in database
+    };
+
+    return (
+        <div>
+            <button onClick={initiatePayment}>Pay with PayHere</button>
+            {paymentStatus && <p>{paymentStatus}</p>}
+        </div>
+    );
+};
+
+export default Payment;
