3e732dc893452584fc2770c0902e8125fd46c064
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-04-16 22:25:27 +0530

    #402 Minor Change

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,14 +10,17 @@ const VerifyEmail = lazy(() => import("@app_pages/user/verify/VerifyEmail"));
 const PlaceOrder = lazy(() => import("@app_pages/customer/order/Order"));
 const Category = lazy(() => import("@app_pages/dashboard/category/Category"));
 const Country = lazy(() => import("@app_pages/dashboard/country/Country"));
-const RestrictedOrderType = lazy(() => import("@app_pages/dashboard/restrictedOrder/RestrictedOrderType"));
-
+const RestrictedOrderType = lazy(
+  () => import("@app_pages/dashboard/restrictedOrder/RestrictedOrderType")
+);
 
 //Order Dashboard
 const Order = lazy(() => import("@app_pages/dashboard/order/Order"));
 
 //Employee
-const EmployeeCheckpoint = lazy(() => import("@app_pages/employee/signin/EmployeeSignIn"));
+const EmployeeCheckpoint = lazy(
+  () => import("@app_pages/employee/signin/EmployeeSignIn")
+);
 
 //Bulk
 const Bulk = lazy(() => import("@app_pages/transport/Bulk"));
@@ -28,93 +31,94 @@ const Flight = lazy(() => import("@app_pages/transport/Flight"));
 //Airline
 const Airline = lazy(() => import("@app_pages/transport/Airline"));
 
-
 //Bulk Details
 const BulkDetails = lazy(() => import("@app_pages/transport/BulkDetails"));
 
 //Order Aggregation
-const OrderAggregation = lazy(() => import("@app_pages/transport/OrderAggregation"));
+const OrderAggregation = lazy(
+  () => import("@app_pages/transport/OrderAggregation")
+);
 
 //Common
 const NotFound = lazy(() => import("@app_pages/common/PageNotFound"));
 
 export const privateRoutes = [
-    {
-        path: "/order",
-        component: PlaceOrder,
-        isPrivate: true,
-    },    
-    {
-        path: "/app/order",
-        component: Order,
-        isPrivate: true,
-    },
-    {
-        path: "/app/bulk",
-        component: Bulk,
-        isPrivate: true,
-    },
-    {
-        path: "/app/flight",
-        component: Flight,
-        isPrivate: true,
-    },
-    {
-        path: "/app/airline",
-        component: Airline,
-        isPrivate: true,
-    },
-    {
-        path: "/app/bulk-details",
-        component: BulkDetails,
-        isPrivate: true,
-    },
-    {
-        path: "/app/order-aggregation",
-        component: OrderAggregation,
-        isPrivate: true,
-    },
-    {
-        path: "/app/restricted-order-type",
-        component: RestrictedOrderType,
-        isPrivate: true,
-    },
-    {
-        path: "/app/category",
-        component: Category,
-        isPrivate: true,
-    },
-    {
-        path: "/app/country",
-        component: Country,
-        isPrivate: true,
-    }
+  {
+    path: "/order",
+    component: PlaceOrder,
+    isPrivate: true,
+  },
+  {
+    path: "/app/order",
+    component: Order,
+    isPrivate: true,
+  },
+  {
+    path: "/app/bulk",
+    component: Bulk,
+    isPrivate: true,
+  },
+  {
+    path: "/app/flight",
+    component: Flight,
+    isPrivate: true,
+  },
+  {
+    path: "/app/airline",
+    component: Airline,
+    isPrivate: true,
+  },
+  {
+    path: "/app/bulk-details",
+    component: BulkDetails,
+    isPrivate: true,
+  },
+  {
+    path: "/app/order-aggregation",
+    component: OrderAggregation,
+    isPrivate: true,
+  },
+  {
+    path: "/app/restricted-order-type",
+    component: RestrictedOrderType,
+    isPrivate: true,
+  },
+  {
+    path: "/app/category",
+    component: Category,
+    isPrivate: true,
+  },
+  {
+    path: "/app/country",
+    component: Country,
+    isPrivate: true,
+  },
 ];
 
 export const publicRoutes = [
-    {
-        path: "/",
-        component: Home,
-        isPrivate: false,
-    },
-    {
-        path: "/home",
-        component: Home,
-        isPrivate: false,
-    },
-    {
-        path: "/verify-email/:token",
-        component: VerifyEmail,
-        isPrivate: false,
-    },
-    {
-        path: "/emp_checkpoint",
-        component: EmployeeCheckpoint,
-        isPrivate: false,
-    },
-    {
-        path: "*",
-        component: NotFound,
-        isPrivate: false,
-    },
+  {
+    path: "/",
+    component: Home,
+    isPrivate: false,
+  },
+  {
+    path: "/home",
+    component: Home,
+    isPrivate: false,
+  },
+  {
+    path: "/verify-email/:token",
+    component: VerifyEmail,
+    isPrivate: false,
+  },
+  {
+    path: "/emp-checkpoint",
+    component: EmployeeCheckpoint,
+    isPrivate: false,
+  },
+  {
+    path: "*",
+    component: NotFound,
+    isPrivate: false,
+  },
 ];
