921d1ac316d9fedc12ef9f84ca6a431aa31c8087
Teran Sarathchandra <teran8777@gmail.com>
2024-05-01 21:14:58 +0530

    #422 Order Placement Side Navigation Steps Disabled

--- src/components/shared/SideDrawer.tsx
+++ src/components/shared/SideDrawer.tsx
@@ -4,16 +4,18 @@ import logo from "@app_assets/images/logo.png"
 type SideDrawerProps = {
     steps: string[];
     currentStep: number;
-    onStepClick: (stepIndex: number) => void;
+    // onStepClick: (stepIndex: number) => void;
 }
 
-const SideDrawer: React.FC<SideDrawerProps> = ({ steps, currentStep, onStepClick }) => {
+// const SideDrawer: React.FC<SideDrawerProps> = ({ steps, currentStep, onStepClick }) => {
+const SideDrawer: React.FC<SideDrawerProps> = ({ steps, currentStep }) => {
     return (
         <Drawer>
             <HeaderImage src={logo}></HeaderImage>
             <div style={{ padding: '2rem 0', backgroundColor: '#FFFFFF55', margin: '1rem .5rem', borderRadius: '15px' }}>
                 {steps.map((step, index) => (
-                    <Step key={step} isActive={index === currentStep} onClick={() => onStepClick(index)}>
+                    // <Step key={step} isActive={index === currentStep} onClick={() => onStepClick(index)}>
+                    <Step key={step} isActive={index === currentStep}>
                         <StepIndicator isActive={index === currentStep} />
                         {step}
                     </Step>
