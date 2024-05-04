fd69edc7e4f94aedb9b97b72233b57b12e9356c8
Dasuni Mandini <dasunimandini@gmail.com>
2024-05-04 21:16:13 +0530

    #422 Code Refactor

--- src/components/dashboard/transport/AirlineInfo.tsx
+++ src/components/dashboard/transport/AirlineInfo.tsx
@@ -10,6 +10,9 @@ import EditDialog from "@app_components/dialog/EditDialog";
 import { UpdateBtn } from "@app_styles/bulkDetails.styles";
 import AddDialog from "@app_components/dialog/AddDialog";
 import DeleteDialog from "@app_components/dialog/DeleteDialog";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addAirlineSchema } from "@app_schemas/bulk/addAirline.Schema";
 // import { useForm } from "react-hook-form";
 // import { yupResolver } from "@hookform/resolvers/yup";
 // import { addAirlineSchema } from "@app_schemas/bulk/addAirline.Schema";
@@ -34,16 +37,16 @@ const AirlineInfo: React.FC = () => {
   const [isAddAirlineOpen, setIsAddAirlineOpen] = useState(false);
   const [currentAirline, setCurrentAirline] = useState<IAirline | null>(null);
 
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-// } = useForm({
-//     resolver: yupResolver(addAirlineSchema),
-// });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(addAirlineSchema),
+  });
 
   const handleEditClick = (airline: IAirline) => {
-    console.log("Airline" , airline);
+    console.log("Airline", airline);
     setCurrentAirline(airline);
     setIsDialogOpen(true);
   };
@@ -52,7 +55,7 @@ const AirlineInfo: React.FC = () => {
   };
 
   const handleDeleteClick = (airline: IAirline) => {
-    console.log("Airline" , airline);
+    console.log("Airline", airline);
     setCurrentAirline(airline);
     setisDeleteDialogOpen(true);
   };
@@ -67,7 +70,7 @@ const AirlineInfo: React.FC = () => {
         edit: <button onClick={() => handleEditClick(airline)} style={{ all: 'unset' }}><FontAwesomeIcon icon={faPen} style={{ cursor: "pointer", color: "#23a840" }} /></button>,
         delete: <button onClick={() => handleDeleteClick(airline)} style={{ all: 'unset' }}><FontAwesomeIcon icon={faTrash} style={{ cursor: "pointer", color: "#dd0426" }} /></button>,
       }));
-       console.log('Current Airline:', currentAirline);
+      console.log('Current Airline:', currentAirline);
       setAirlne(preparedAirlines);
     } catch (error) {
       console.error('Failed to fetch airlines', error);
@@ -81,21 +84,21 @@ const AirlineInfo: React.FC = () => {
   const saveAirline = async (airlineData) => {
     console.log('Saving Airline:', airlineData);
     setIsDialogOpen(false);
-  try {
-    const airlineId = currentAirline?._id;
-    if (airlineId) {
-      await updateAirline(airlineId, airlineData); 
-      console.log('Airline details updated successfully');
+    try {
+      const airlineId = currentAirline?._id;
+      if (airlineId) {
+        await updateAirline(airlineId, airlineData);
+        console.log('Airline details updated successfully');
 
-      
-      fetchAndPrepareAirlines();
+
+        fetchAndPrepareAirlines();
+      }
+      setIsDialogOpen(false);
+    } catch (error) {
+      console.error('Failed to update airline details', error);
+
     }
-    setIsDialogOpen(false); 
-  } catch (error) {
-    console.error('Failed to update airline details', error);
-    
-  }
-};
+  };
 
   const addAirline = async (airlineData) => {
     try {
@@ -146,7 +149,7 @@ const AirlineInfo: React.FC = () => {
         onSave={saveAirline}
         onDelete={deleteAirline}
       />
-     <UpdateBtn onClick={handleAddClick}>Add Airline</UpdateBtn>
+      <UpdateBtn onClick={handleAddClick}>Add Airline</UpdateBtn>
       <AddDialog
         isOpen={isAddAirlineOpen}
         handleClose={() => setIsAddAirlineOpen(false)}
@@ -155,13 +158,13 @@ const AirlineInfo: React.FC = () => {
           //{ name: 'flightId', label: 'Flight No', type: 'text', disabled: false },
           { name: 'code', label: 'Code', type: 'text', disabled: false },
           { name: "name", label: "Name", type: 'text', disabled: false },
-          
+
         ]}
         onSave={addAirline}
       />
       <DeleteDialog
-        isOpen= {isDeleteDialogOpen}
-        handleClose={() => setisDeleteDialogOpen(false)}        
+        isOpen={isDeleteDialogOpen}
+        handleClose={() => setisDeleteDialogOpen(false)}
         handleDelete={handleDeleteAirline}
       />
     </>
